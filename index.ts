91c906624b8402cee8ce283a3cb5eb7798cad71a
Harish Sunkerikar <h.sunkerikar@prowareness.nl>
2018-11-29 15:54:44 +0530

    Instantiaiting a new Observable

--- index.ts
+++ index.ts
@@ -1,1 +1,20 @@
-alert("Hello");
+import { Observable } from "rxjs";
+import { allBooks } from "./data";
+
+let allBooksObservable$ = Observable.create(subscriber => {
+  if (document.title !== "RxBookTracker") {
+    subscriber.error("Incorrect Page Title");
+  }
+  for (let book of allBooks) {
+    subscriber.next(book);
+  }
+
+  setTimeout(() => {
+    subscriber.complete();
+  }, 3000);
+  return () => {
+    console.log("Executing Tear down code");
+  };
+});
+
+allBooksObservable$.subscribe(book => console.log(book.title));
